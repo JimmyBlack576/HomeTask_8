17e4fd26b0dcd8230587cf156a8320a6309a9a05
Jimmy Blackwood <Jimmy Blackwood@DESKTOP-AOM0ABL>
2022-05-15 13:16:20 +0300

    Добавьте файлы проекта.

---
+++ HomeTask_8/HomeTask_8.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <Windows.h>
+
+using namespace std;
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    double summa, proc;
+    cout << "Введите сумму вклада: \n";
+    cin >> summa;
+    cout << "Введите ставку в процентах: \n";
+    cin >> proc;
+
+    proc = (proc / 100) / 12;
+    cout << "Кажый месяц вы получите: " << summa * proc << endl;
+
+    return 0;
+
+}
+
+
